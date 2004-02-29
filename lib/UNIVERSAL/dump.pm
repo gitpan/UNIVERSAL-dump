0fe16312783b52baf0aa6feded8ac4c5a7ab33ba
Elizabeth Mattijsen <liz@dijkmat.nl>
2004-02-29 17:32:34 +0000

    Import of ELIZABETH/UNIVERSAL-dump-0.04 from CPAN.
    gitpan-cpan-distribution: UNIVERSAL-dump
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         ELIZABETH/UNIVERSAL-dump-0.04.tar.gz
    gitpan-cpan-author:       ELIZABETH
    gitpan-cpan-maturity:     released

--- lib/UNIVERSAL/dump.pm
+++ lib/UNIVERSAL/dump.pm
@@ -3,8 +3,17 @@ package UNIVERSAL::dump;
 # Make sure we have version info for this module
 # Be strict from now on
 
-$VERSION = '0.03';
+$VERSION = '0.04';
 use strict;
+
+# Initialize the hash with presets
+
+my %preset = (
+ blessed => 'Scalar::Util::blessed',
+ dump    => 'Data::Dumper::Dumper',
+ peek    => 'Devel::Peek::Dump',
+ refaddr => 'Scalar::Util::refaddr',
+);
 
 # Hash with installed handlers
 
@@ -25,46 +34,61 @@ my %installed;
 sub import {
 
 # Obtain the class (ignored for now)
-# Set default method and subroutine name if nothing specified
+# Set default method if nothing specified
 
     my $class = shift;
-    unshift( @_,dump => 'Data::Dumper::Dumper' ) unless @_;
+    unshift( @_,'dump' ) unless @_;
 
 # Allow for redefining subroutines
-# While there are parameters to be handled
-#  Get the next method/subroutine pair
-#  If there is already a method installed with that name
-#   Croak if we're trying to install a different one
+# For all of the parameters to be handled
+#  If we got a preset
+#   Die now if we don't know how to handle the preset
+#   Handle the preset by calling ourselves
+#   And reloop
 
     no warnings 'redefine';
-    while (@_) {
-        my ($method,$sub) = splice @_,0,2;
-        if (my $installed = $installed{$method}) {
-            die qq{Cannot install "UNIVERSAL::$method" with "$sub": already installed with "$installed"\n} if $sub ne $installed;
+    foreach (@_) {
+        unless (ref) {
+            die qq{Don't know how to install method "UNIVERSAL::$_"\n}
+             unless my $sub = $preset{$_};
+            $class->import( {$_ => $sub} );
+            next;
         }
 
-#  Fetch the module name from it
-#  Turn it into something that can be used in a -require-
-#  Mark this method as installed
+#  For all the method / subroutine pairs
+#   Normalize if another method name was specified
+#   If there is already a method installed with that name
+#    Croak if we're trying to install a different one
 
-        (my $module = $sub) =~ s#::[^:]+$##;
-        $module =~ s#::#/#; $module .= ".pm";
-        $installed{$method} = $sub;
+        while (my ($method,$sub) = each %{$_}) {
+            $sub = $preset{$sub} if $preset{$sub};
+            if (my $installed = $installed{$method}) {
+                die qq{Cannot install "UNIVERSAL::$method" with "$sub": already installed with "$installed"\n} if $sub ne $installed;
+            }
 
-#  Allow for variable references to subroutines
-#  Create a method which
-#   Obtains the object / class
-#   Makes sure that we have the right module (even if it doesn't really exists)
-#   Return dumper output if not in void context
-#   Send dumper output to STDERR otherwise
+#   Fetch the module name from it
+#   Turn it into something that can be used in a -require-
+#   Mark this method as installed
 
-        no strict 'refs';
-        *{"UNIVERSAL::$method"} = sub {
-            my $self = shift;
-            eval { require $module };
-            return $sub->( @_ ? @_ : $self ) if defined wantarray;
-            print STDERR $sub->( @_ ? @_ : $self );
-        } #$method
+            (my $module = $sub) =~ s#::[^:]+$##;
+            $module =~ s#::#/#; $module .= ".pm";
+            $installed{$method} = $sub;
+
+#   Allow for variable references to subroutines
+#   Create a method which
+#    Obtains the object / class
+#    Makes sure that we have the right module (even if it doesn't really exists)
+#    Return dumper output if not in void context
+#    Send dumper output to STDERR otherwise
+
+            no strict 'refs';
+            *{"UNIVERSAL::$method"} = sub {
+                my $self = shift;
+                eval { require $module };
+                return $sub->( @_ ? @_ : $self ) if defined wantarray;
+                print STDERR $sub->( @_ ? @_ : $self );
+            } #$method
+        }
     }
 } #import
 
@@ -78,20 +102,19 @@ UNIVERSAL::dump - add dump methods to all classes and objects
 
 =head1 SYNOPSIS
 
-  use UNIVERSAL::dump; # implicit dump => 'Data::Dumper::Dumper'
+  use UNIVERSAL::dump; # implicit 'dump'
 
  or:
 
-  use UNIVERSAL::dump (
-   _dump => 'Data::Dumper::Dumper',
-  );
+  use UNIVERSAL::dump qw(dump peek); # create both "dump" and "peek"
 
  or:
 
-  use UNIVERSAL::dump ( # create both "dump" as well as "peek"
-   dump => 'Data::Dumper::Dumper',
-   peek => 'Devel::Peek::Dump',
-  );
+  use UNIVERSAL::dump ( { _dump => 'dump' } ); # dump using "_dump"
+
+ or:
+
+  use UNIVERSAL::dump ( { bar => 'Bar::Dumper' } ); # "foo" dumper
 
  my $foo = Foo->new;
  print $foo->dump;         # send dump of $foo to STDOUT
@@ -109,14 +132,44 @@ add and remove debugging code from modules and programs.
 By default, it adds a method "dump" to all classes and objects.  This method
 either dumps the object, or any parameters specified, using L<Data::Dumper>.
 
-The name of the method, and the name of the subroutine to be called, can be
-specified by parameters when loading the module.  One example is creating a
-"peek" method that will us L<Devel::Peek> to dump the contents of the object
-or anything else passed to the dump method.
-
 As an extra feature, the output is sent to STDERR whenever the method is
 called in a void context.  This makes it easier to dump variable structures
 while debugging modules and programs.
+
+The name of the method can be specified by parameters when loading the module.
+These are the method names that are currently recognized:
+
+=over 2
+
+=item blessed
+
+Return or prints with which class the object (or any value) is blessed.
+Uses L<Scalar::Util>'s "blessed" subroutine.
+
+=item dump
+
+Return or prints a representation of the object (or any value that is
+specified).  Uses L<Data::Dumper>'s "Dumper" subroutine.
+
+=item peek
+
+Return or prints the internal representation of the object (or any value that
+is specified).  Uses L<Devel::Peek>'s "Dump" subroutine.
+
+=item refaddr
+
+Return or prints with the memory address of the object (or any value
+specified).  Uses L<Scalar::Util>'s "refaddr" subroutine.
+
+=back
+
+If you cannot use one of the preset names of methods, you can specify a
+reference to a hash instead, in which the key is the new name of the method
+and the value is the name with which the dumping method is normally indicated.
+
+If you have a dumping subroutine that is not available by default, you can
+add your own by specifying a reference to a hash, in which the key is the
+method name, and the value is the (fully qualified) name of the subroutine.
 
 To prevent different modules fighting over the same method name, a check
 has been built in which will cause an exception when the same method is
@@ -127,22 +180,6 @@ attempted with a different subroutine name.
 One day, I finally had enough of always putting a "dump" and "peek" method
 in my modules.  I came across L<UNIVERSAL::moniker> one day, and realized
 that I could do something similar for my "dump" methods.
-
-=head1 MORE EXOTIC USES
-
-This module can be used for more exotic uses as well.
-
-=head2 blessed
-
- use UNIVERSAL::dump blessed => 'Scalar::Util::blessed';
-
- if ($foo->blessed eq 'Foo') {
-    print "This is a Foo object\n";
- }
-
-Adds the "blessed" method to all objects.  Returns the class with which the
-the object or specified value is not blessed.  Returns undef in every other
-case.  Prints the return value to STDERR if called in void context.
 
 =head1 REQUIRED MODULES
 
--- t/dump01.t
+++ t/dump01.t
@@ -6,28 +6,31 @@ BEGIN {				# Magic Perl CORE pragma
     }
 }
 
-use Test::More tests => 6;
+use Test::More tests => 10;
 use strict;
 use warnings;
 
-use_ok( 'UNIVERSAL::dump' );
+my @preset = qw(blessed dump peek refaddr);
+use_ok( 'UNIVERSAL::dump',@preset );
+can_ok( 'UNIVERSAL',@preset );
 
-sub Foo::testing { $_[0] }
-use UNIVERSAL::dump (testing => 'Foo::dump');
+sub Foo::testing { "Testing $_[0]" }
+UNIVERSAL::dump->import( { testing => 'Foo::testing' } );
+can_ok( 'UNIVERSAL','testing' );
 
-can_ok( 'UNIVERSAL::dump',qw(
- dump
- import
- testing
-) );
+UNIVERSAL::dump->import( { _dump   => 'dump' } );
+can_ok( 'UNIVERSAL','_dump' );
 
 require Data::Dumper;
 my $foo = bless {},'Foo';
 is( $foo->dump,Data::Dumper::Dumper( $foo ),"Check if Data::Dumper dump ok" );
-is( $foo->testing,Foo::testing( $foo ),"Check if Foo dump ok" );
+is( $foo->_dump,Data::Dumper::Dumper( $foo ),"Check if Data::Dumper _dump ok" );
 
-eval { UNIVERSAL::dump->import( testing => 'Foo::otherdump' ) };
+is( $foo->testing,Foo::testing( $foo ),"Check if Foo dump ok" );
+is( bar->testing( $foo ),Foo::testing( $foo ),"Check if Foo dump ok" );
+
+eval { UNIVERSAL::dump->import( { testing => 'Foo::otherdump' } ) };
 like( $@,qr#^Cannot install#,"If same method, different sub causes error" );
 
-eval { UNIVERSAL::dump->import( testing => 'Foo::dump' ) };
+eval { UNIVERSAL::dump->import( { testing => 'Foo::testing' } ) };
 is( $@,'',"If same method, same sub does not cause error" );
